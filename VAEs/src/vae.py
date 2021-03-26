e30e954838a5a8dc794faf9e96b13bf9d9055908
scuri <sebastian.curi@inf.ethz.ch>
2021-03-26 11:22:58 +0100

    Add beta-vae parameter as well as flag to constrain the output

--- VAEs/src/vae.py
+++ VAEs/src/vae.py
@@ -5,7 +5,7 @@ import numpy as np
 import networkx as nx
 
 class VAE(nn.Module):
-    def __init__(self, x_dim, hidden_dims, z_dim):
+    def __init__(self, x_dim, hidden_dims, z_dim, beta=0, constrained_output=False):
         super().__init__()
         self.z_dim = z_dim
         
@@ -29,10 +29,13 @@ class VAE(nn.Module):
                 nn.Sequential(nn.Linear(cur, h_dim), nn.ReLU())
             )
             cur = h_dim
-        modules.append( nn.Sequential(nn.Linear(cur, x_dim), nn.Sigmoid() ))
-        self.decoder = nn.Sequential(*modules)
+        modules.append(nn.Sequential(nn.Linear(cur, x_dim)))
 
-        
+        if constrained_output:
+            modules.append(nn.Sigmoid())
+        self.decoder = nn.Sequential(*modules)
+        self.beta = beta
+
     def encode(self, x):
         """Encodes the input using the encoder network.
 
@@ -53,7 +56,6 @@ class VAE(nn.Module):
         logvar = result[:, 1]
         return mu, logvar
 
-    
     def decode(self, z):
         """Decodes the latent codes onto the input space.
 
@@ -69,7 +71,6 @@ class VAE(nn.Module):
         result = self.decoder(z)
         return result
 
-    
     def reparameterize(self, mu, logvar):
         """Reparameterization trick to sample from a Gaussian N(mu, var)
         from N(0, 1)
@@ -91,13 +92,11 @@ class VAE(nn.Module):
         eps = torch.randn_like(std)
         return eps * std + mu
 
-    
     def forward(self, x):
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-    
     def loss(self, x, recons, mu, logvar, criterion):
         """Computes the loss function."""
 
@@ -106,7 +105,5 @@ class VAE(nn.Module):
 
         # Kl-Divergence
         kl_loss = -0.5 * torch.sum(1 + logvar - mu**2 - logvar.exp(), dim=1)
-
-        loss = rec_loss + kl_loss
+        loss = rec_loss + self.beta * kl_loss
         return loss
-        
