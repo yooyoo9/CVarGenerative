217ba5f0b34674104d083531069d3d36e09be7a8
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-03-26 18:09:00 +0100

    Correct Implementation of CVaR_EM

--- GMMs/src/cvar_em.py
+++ GMMs/src/cvar_em.py
@@ -1,39 +1,74 @@
 import numpy as np
+import os
+
 from sklearn.mixture import GaussianMixture
 
-np.random.seed(176371)
+from adacvar.util.adaptive_algorithm import Exp3
 
-class CVaR_EM(GaussianMixture):
-    def __init__(self, n_components=1, *, covariance_type='full', tol=1e-3,
-                 reg_covar=1e-6, max_iter=100, n_init=1, init_params='kmeans',
-                 weights_init=None, means_init=None, precisions_init=None,
-                 random_state=None, warm_start=False,
-                 verbose=0, verbose_interval=10, k=150, epsilon=1e-2):
-        super().__init__(
-            n_components=n_components, covariance_type=covariance_type,
-            tol=tol, reg_covar=reg_covar, max_iter=max_iter, n_init=n_init,
-            init_params=init_params, weights_init=weights_init,
-            means_init=means_init, precisions_init=precisions_init,
-            random_state=random_state, warm_start=warm_start,
-            verbose=verbose, verbose_interval=verbose_interval)
 
-        self.k = k
-        self.epsilon = epsilon
-    
-    def _m_step(self, X, log_resp):
-        # weights = pi_c
-        # log_resp: n_samples * n_components matrix, log P(c | xi)
-        # estimate_log_prob: returns log P(x | theta)
-        # n_samples, n_components = log_prob.shape
-        old = new = 0
-        while True:
-            old = new
-            log_prob = self._estimate_log_prob(X)
-            weights = np.log(np.broadcast_to(self.weights_, log_prob.shape))
-            log_marginal_prob = log_resp @ (log_prob + weights).T
-            ind = np.argsort(np.sum(log_marginal_prob, axis=1))[:self.k]
-            new = np.sum(log_marginal_prob[ind])
-            if np.abs(new - old) < self.epsilon:
-                break
-            super()._m_step(X[ind], log_resp[ind])
+class CVaR_EM:
+    """Implementation of the CVaR_EM algorithm.
 
+    Parameters
+    ----------
+    n_components: int
+        Number of clusters of the mixture.
+    epochs: int
+        Number of epochs to train.
+    num_actions: int
+        Number of datapoints in dataset.
+    size: int
+        Number of datapoints to select at each iteration.
+    eta: callable or float
+        Learning rate scheduler
+    gamma: float, optional
+        Mixing of uniform distribution.
+
+    Methods
+    -------
+    sample: Adaptive data sampler
+    fit_predict: Fit the data using the CVaR_EM algorithm and predicts otuput.
+    """
+
+    def __init__(self, n_components, epochs, num_actions, size, eta, gamma=0.0):
+        self.num_actions = num_actions
+        self.size = size
+        self.gm = GaussianMixture(
+            n_components=n_components,
+            covariance_type="full",
+            tol=1e-3,
+            max_iter=100,
+            n_init=10,  # increase
+            init_params="kmeans",  # kmeans or random
+        )
+        self.epochs = epochs
+        self._exp3 = Exp3(num_actions, size, eta, gamma)
+
+    def sample(self):
+        weights = self._exp3.probabilities
+        idx = np.random.choice(
+            self.num_actions,
+            self.size,
+            p=weights / np.sum(weights),
+            replace=False,
+        )
+        return idx
+
+    def fit_predict(self, X):
+        idx = np.zeros(self.size)
+        last = np.ones(self.size)
+        cnt = 0
+        for i in range(self.epochs):
+            cnt += 1
+            last = idx
+            idx = np.sort(self.sample())
+            curX = X[idx]
+            self.gm.fit(curX)
+            # negative log likelihood of the Gaussian mixture given X
+            loss = np.clip((-self.gm.score_samples(curX) / 20 + 1) / 2, 0, 1)
+
+            weights = 1.0
+            prob = self._exp3.probabilities
+            self._exp3.update(loss, idx, prob)
+            self._exp3.normalize()
+        return self.gm.predict(X)
--- GMMs/src/train.py
+++ GMMs/src/train.py
@@ -1,83 +1,122 @@
 import numpy as np
-from sklearn import cluster, datasets
-from sklearn.preprocessing import StandardScaler
-from sklearn.mixture import GaussianMixture
-
+import os
 import matplotlib.pyplot as plt
+
+from sklearn import cluster, datasets
+from sklearn.mixture import GaussianMixture
 from itertools import cycle, islice
-from adacvar_em import AdaCVaR_EM
+
+from cvar_em import CVaR_EM
 
 np.random.seed(31415)
 
-param = {'n_samples': 1500,
-         'n': 100,
-         'threshold': 10,
-         }
+param = {
+    "alpha": 0.3,
+    "epochs": 10,
+    "n_samples": 200,
+    "n": 10,
+    "path_X": "../data_X.npy",
+    "path_y": "../data_y.npy",
+    "path_out": "../output/",
+}
 
-def label(y, n_clusters):
-    ind=0
-    labels = dict()
-    for i in range(len(y)):
-        if y[i] in labels.keys():
-            y[i] = labels[y[i]]
-        else:
-            labels[y[i]] = y[i] = ind
-            ind += 1
-    return y
 
-def predict(algorithm, X):
-    best_seed = -1
-    best_score = -1e7
-    for i in range(10):
-        seed = np.random.randint(1, 1e5)
-        algorithm.set_params(random_state =seed)
-        algorithm.fit(X)
-        score = np.sum(algorithm.score(X))
-        if best_score < score:
-            best_score = score
-            best_seed = seed
-    algorithm.set_params(random_state = best_seed)
-    algorithm.fit(X)
-    return algorithm.predict(X)
+def generate(n, n_samples, pathX, pathy):
+    """Generates the dataset and saves it as a numpy array.
+
+    Parameters
+    ----------
+    n: int
+        Number of datasets to generate
+    n_samples: int
+        Number of datapoints per dataset
+    pathX: string
+        Path to save the dataset
+    pathy: string
+        Path to save the clusters of the correponding datapoints
+    """
+
+    data = np.empty((n, n_samples + 1, 2))
+    Y = np.empty((n, n_samples))
+    for i in range(n):
+        k = np.random.randint(2, 5)
+        std = 2 * np.random.rand(k)
+        X, y = datasets.make_blobs(n_samples=n_samples, centers=k, cluster_std=std)
+        # Normalize the data
+        X -= X.mean()
+        X /= X.std()
+
+        data[i, :-1] = X
+        data[i, -1] = [k, 0]
+        Y[i] = y
+    np.save(pathX, data)
+    np.save(pathy, Y)
+
+
+# Check if data already present, if not generate
+if not os.path.isfile(param["path_X"]):
+    generate(param["n"], param["n_samples"], param["path_X"], param["path_y"])
+X = np.load(param["path_X"])
+y = (np.load(param["path_y"])).astype(int)
 
 fig = plt.figure(figsize=[10, 6])
-for i in range(param['n']):
-    # if i%10 == 0:
-    print(i)
-    n_clusters = np.random.randint(1, 6)
+for i in range(param["n"]):
+    curX = X[i, :-1]
+    cury = y[i]
+    n_clusters = int(X[i, -1, 0])
 
-    std = 4.0 * np.random.rand(n_clusters)
-    sample_distr = np.random.rand(n_clusters)
-    sample_distr *= param['n_samples'] / np.sum(sample_distr)
-    sample_distr = (np.round(sample_distr)).astype(int)
-    X, y = datasets.make_blobs(n_samples = sample_distr, cluster_std=std)
-    X = StandardScaler().fit_transform(X)
+    cvar = CVaR_EM(
+        n_components=n_clusters,
+        epochs=param["epochs"],
+        num_actions=param["n_samples"],
+        size=int(np.ceil(param["alpha"] * param["n_samples"])),
+        eta=np.sqrt(1 / param["alpha"] * np.log(1 / param["alpha"])),
+    )
 
-    cvar = AdaCVaR_EM(
-        max_iter=1000, n_components=n_clusters, covariance_type='full')
     gmm = GaussianMixture(
-        n_components=n_clusters, covariance_type='full')
-    cvar_y = label(predict(cvar, X), n_clusters)
-    gmm_y = label(predict(gmm, X), n_clusters)
+        n_components=n_clusters,
+        covariance_type="full",
+        tol=1e-3,
+        max_iter=100,
+        n_init=10,
+        init_params="kmeans",
+    )
 
-    if np.linalg.norm(cvar_y-gmm_y, 0) > param['threshold']:
-        colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
-                                             '#f781bf', '#a65628', '#984ea3',
-                                             '#999999', '#e41a1c', '#dede00']),
-                                     int(max(max(cvar_y), max(gmm_y)) + 1))))
-        # add black color for outliers (if any)
-        colors = np.append(colors, ["#000000"])
-        ax = plt.subplot(1, 3, 1)
-        ax.set_title("True distribution")
-        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[y])
-        
-        ax1 = plt.subplot(1, 3, 2)
-        ax1.set_title("GMM")
-        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[gmm_y])
+    cvar_y = cvar.fit_predict(curX)
+    gmm_y = gmm.fit_predict(curX)
 
-        ax2 = plt.subplot(1, 3, 3)
-        ax2.set_title("CVaR_EM")
-        plt.scatter(X[:, 0], X[:, 1], s=10, color=colors[cvar_y])
+    colors = np.array(
+        list(
+            islice(
+                cycle(
+                    [
+                        "#377eb8",
+                        "#ff7f00",
+                        "#4daf4a",
+                        "#f781bf",
+                        "#a65628",
+                        "#984ea3",
+                        "#999999",
+                        "#e41a1c",
+                        "#dede00",
+                    ]
+                ),
+                int(max(max(cvar_y), max(gmm_y)) + 1),
+            )
+        )
+    )
+    # add black color for outliers (if any)
+    colors = np.append(colors, ["#000000"])
 
-        plt.savefig('../output/'+str(i)+'.png')
-        plt.clf()
+    # Plot the result
+    ax = plt.subplot(1, 3, 1)
+    ax.set_title("True distribution")
+    ax.scatter(curX[:, 0], curX[:, 1], s=10, color=colors[cury])
+    ax1 = plt.subplot(1, 3, 2)
+    ax1.set_title("GMM")
+    ax1.scatter(curX[:, 0], curX[:, 1], s=10, color=colors[gmm_y])
+    ax2 = plt.subplot(1, 3, 3)
+    ax2.set_title("CVaR_EM")
+    ax2.scatter(curX[:, 0], curX[:, 1], s=10, color=colors[cvar_y])
+    plt.savefig(param["path_out"] + str(i) + ".png")
+    plt.clf()
