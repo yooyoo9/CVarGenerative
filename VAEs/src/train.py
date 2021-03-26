512bb55499d25c51ed9c25811d4a36234e24d018
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-03-26 15:47:22 +0100

    Merged usual and cvar VAE alg

---
+++ VAEs/src/train.py
@@ -0,0 +1,191 @@
+import numpy as np
+import os
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+
+from adacvar.util.cvar import CVaR
+from adacvar.util.adaptive_algorithm import Exp3Sampler
+
+from vae import VAE
+
+
+class VAEalg:
+    def __init__(
+        self, model_path, model_param, train_set, valid_set, batch_size, lr, criterion
+    ):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        if os.path.exists(model_path):
+            self.model = torch.load(model_path)
+        else:
+            self.model = VAE(
+                x_dim=model_param["x_dim"],
+                hidden_dims=model_param["hidden_dims"],
+                z_dim=model_param["z_dim"],
+                beta=model_param["beta"],
+            )
+        self.model_vae.to(self.device)
+
+        self.train_loader = DataLoader(train_set, batch_size)
+        self.val_loader = DataLoader(valid_set, batch_size)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.criterion = criterion
+
+    def train(self, epochs, save_model):
+        """Trains the VAE using the training set
+
+        Parameters
+        ----------
+        epochs: int
+            Number of epochs to train
+        save_model: bool
+            If set to true, saves the model after training
+        """
+        self.model.train()
+        for epoch_idx in range(epochs):
+            print(f"Epoch {epoch_idx+1} of {epochs}")
+            running_loss = 0.0
+            for batch_idx, (data, *_) in enumerate(self.train_loader):
+                data = data.to(self.device)
+                data = data.view(data.size(0), -1)
+                self.optimizer.zero_grad()
+                recons, mu, logvar = self.model(data)
+                loss = self.model.loss(data, recons, mu, logvar, self.criterion)
+                loss = loss.sum()
+                running_loss += loss.item()
+                loss.backward()
+                self.optimizer.step()
+            train_loss = running_loss / len(self.train_loader.dataset)
+            val_loss = self.evaluate()
+            print(f"Train Loss: {train_loss:.4f}   Val Loss: {val_loss:.4f}")
+        if save_model:
+            torch.save(self.model, self.model_path)
+
+    def evaluate(self):
+        """Evaluates the VAE using the validation data.
+
+        Returns
+        -------
+        val_loss: float
+            The validation loss
+        """
+        self.model.eval()
+        running_loss = 0.0
+        with torch.no_grad():
+            for (data, *_) in self.val_loader:
+                data = data.to(self.device)
+                data = data.view(data.size(0), -1)
+                recons, mu, logvar = self.model(data)
+                loss = self.model.loss(data, recons, mu, logvar, self.criterion)
+                loss = loss.sum()
+                running_loss += loss.item()
+            val_loss = running_loss / len(self.val_loader.dataset)
+        return val_loss
+
+
+class CVaRalg(VAEalg):
+    def __init__(
+        self,
+        model_path,
+        model_param,
+        train_set,
+        valid_set,
+        batch_size,
+        lr,
+        criterion,
+        alpha,
+    ):
+        super().__init__(
+            model_path, model_param, train_set, valid_set, batch_size, lr, criterion
+        )
+
+        self.exp3 = Exp3Sampler(
+            batch_size,
+            num_actions=len(train_set),
+            size=int(np.ceil(alpha * len(train_set))),
+            eta=np.sqrt(1 / alpha * np.log(1 / alpha)),
+            gamma=0,
+            beta=0,
+            eps=0,
+            iid_batch=False,
+        )
+        self.train_loader = DataLoader(train_set, batch_sampler=self.exp3)
+        self.cvar = CVaR(alpha=1, learning_rate=0).to(self.device)
+        self.k = int(np.ceil(alpha * len(self.val_loader.dataset)))
+
+    def train(epochs, save_model):
+        """Trains the CVaR VAE using the training set
+
+        Parameters
+        ----------
+        epochs: int
+            Number of epochs to train
+        save_model: bool
+            If set to true, saves the model after training
+        """
+        for epoch_idx in range(epochs):
+            print(f"Epoch {epoch_idx+1} of {epochs}")
+            self.model.train()
+            running_loss = 0.0
+            for batch_idx, (data, idx) in enumerate(self.train_loader):
+                data = data.to(self.device)
+                data = data.view(data.size(0), -1)
+                self.optimizer.zero_grad()
+                self.cvar.zero_grad()
+
+                recons, mu, logvar = self.model(data)
+                loss = self.model.loss(data, recons, mu, logvar, self.criterion)
+
+                weights = 1.0
+                prob = self.exp3.probabilities
+                self.exp3.update(
+                    1 - np.clip(loss.cpu().detach().numpy(), 0, 1), idx, prob
+                )
+
+                cvar_loss = (
+                    torch.tensor(weights).float().to(self.device) * self.cvar(loss)
+                ).mean()
+                running_loss += loss.sum().item()
+                loss = loss.mean()
+                cvar_loss.backward()
+
+                self.optimizer.step()
+                self.cvar.step()
+
+                self.exp3.normalize()
+
+            train_loss = running_loss / len(self.train_loader.dataset)
+            val_loss = self.evaluate()
+            print(f"Train Loss: {train_loss:.4f}   Val Loss: {val_loss:.4f}")
+        if model_save:
+            torch.save(self.model, self.path)
+
+    def evaluate(self):
+        """Evaluates the CVaR VAE using the validation data.
+
+        Returns
+        -------
+        val_loss: float
+            The validation loss
+        """
+        self.model.eval()
+        top_k = None
+        count = 0
+        running_loss = 0.0
+        with torch.no_grad():
+            for data, _ in self.val_loader:
+                count += data.shape[0]
+                data = data.view(data.size(0), -1)
+                recons, mu, logvar = self.model(data)
+                losses = self.model.loss(data, recons, mu, logvar, self.criterion).sort(
+                    descending=True
+                )[0]
+                if top_k is None:
+                    top_k = losses[: self.k]
+                else:
+                    top_k = (torch.cat((top_k, losses))).sort(descending=True)[0]
+                    top_k = top_k[: self.k]
+                running_loss += losses.sum().item()
+            val_loss = running_loss / len(self.val_loader.dataset)
+        return val_loss
