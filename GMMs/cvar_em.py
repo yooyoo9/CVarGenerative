2ad7de8a72fbb0150c394f7074b6a28c6ecbf140
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-04-13 09:48:53 +0200

    Added kdpp code

--- GMMs/cvar_em.py
+++ GMMs/cvar_em.py
@@ -1,12 +1,15 @@
 import numpy as np
-import os
 
 from sklearn.mixture import GaussianMixture
+from scipy.stats import multivariate_normal
 
-from adacvar.util.adaptive_algorithm import Exp3
+from adacvar.util.adaptive_algorithm import Hedge
+
+import matplotlib.pyplot as plt
+from itertools import cycle, islice
 
 
-class CVaR_EM:
+class CVarEM:
     """Implementation of the CVaR_EM algorithm.
 
     Parameters
@@ -16,9 +19,9 @@ class CVaR_EM:
     n_init: int
         Number of initializations for the Gaussian Mixture algorithm.
     num_actions: int
-        Number of datapoints in dataset.
+        Number of data points in dataset.
     size: int
-        Number of datapoints to select at each iteration.
+        Number of data points to select at each iteration.
     lr: float
         Learning rate of the Hedge algorithm.
 
@@ -28,7 +31,7 @@ class CVaR_EM:
     fit_predict: Fit the data using the Hedge algorithm and predicts output.
     """
 
-    def __init__(self, n_components, n_init, num_actions, size, lr=0.01):
+    def __init__(self, n_components, n_init, num_actions, size, lr=0.2):
         self.num_actions = num_actions
         self.size = size
         self.gm = GaussianMixture(
@@ -39,28 +42,92 @@ class CVaR_EM:
             n_init=n_init,
             init_params="kmeans",  # kmeans or random
         )
-        self.lr = lr
+        self.hedge = Hedge(
+            num_actions=num_actions,
+            size=size,
+            eta=lr
+        )
 
-    def fit_predict(self, X):
+    def negative_log_likelihood(self, data):
+        weights = self.gm.weights_
+        means = self.gm.means_
+        cov = self.gm.covariances_
+        m = len(weights)
+        n = len(data)
+        
+        ll = np.empty(n)
+        for i in range(n):
+            ll[i] = sum([weights[j] * multivariate_normal.pdf(data[i], means[j], cov[j]) for j in range(m)])
+        ll = np.log(ll)
+        return -ll
+
+    def fit_predict(self, data):
+        self.gm.fit(data)
+        nll = self.negative_log_likelihood(data)
+        clip_max = 5*np.max(nll)
+
         idx = np.zeros(self.size)
         last_idx = np.ones(self.size)
-        weights = np.ones(self.num_actions)
         loss = []
+        cvar_loss = []
+        cnt = 0
         while not np.array_equal(idx, last_idx):
+            cnt += 1
             last_idx = idx
-            probabilities = weights / np.sum(weights)
+            prob = self.hedge.probabilities
+            prob /= np.sum(prob)
             idx = np.random.choice(
                 np.arange(self.num_actions),
                 self.size,
-                False,  # with or without replacement
-                probabilities,
+                p=prob,
+                replace=False
             )
             idx = np.sort(idx)
-            curX = X[idx]
-
-            self.gm.fit(curX)
+            self.gm.fit(data[idx])
             # weighted log probabilities for each sample in X
-            cost = -self.gm.score_samples(X)
-            loss += [np.sum(cost)]
-            weights *= (1 - self.lr) ** cost
-        return self.gm.predict(X), loss, weights
+            nll = self.negative_log_likelihood(data)
+            cvar_loss += [ np.sum(np.sort(nll)[-self.size:]) ]
+            # print("CVar Loss: " + str(cvar_loss[-1]))
+            cost = nll / clip_max
+            cost = np.clip(cost, 0, 1) 
+            loss += [-self.gm.score(data)]
+            # loss += [np.sum(nll)]
+            # print(abs(np.sum(nll) + self.gm.score(X)))
+            self.hedge.update(cost)
+            self.hedge.normalize()
+            if cnt % 20 == 0:
+                print(str(cnt) + " " + str(loss[-1]) + " " + str(cvar_loss[-1]))
+                ss = np.array([1 if cur <= 1e-4 else 0 for cur in self.hedge.probabilities])
+                print(np.sum(ss))
+                color_ar = [
+                    "#377eb8",
+                    "#ff7f00",
+                    "#4daf4a",
+                    "#f781bf",
+                    "#a65628",
+                    "#984ea3",
+                    "#999999",
+                    "#e41a1c",
+                    "#dede00",
+                ]
+                cycle_nb = 3
+                colors = np.array(list(islice(cycle(color_ar), cycle_nb)))
+                # add black color for outliers (if any)
+                colors = np.append(colors, ["#000000"])
+                cvar_y = self.gm.predict(data)
+                plt.scatter(data[:, 0], data[:, 1], s=10, color=colors[cvar_y])
+                plt.savefig("output/" + str(cnt) + ".png")
+                plt.clf()
+
+                plt.plot(loss)
+                plt.savefig("loss/" + str(cnt) + ".png")
+                plt.clf()
+                plt.plot(cvar_loss)
+                plt.savefig("cvar_loss/" + str(cnt) + ".png")
+                plt.clf()
+                plt.plot(self.hedge.probabilities)
+                plt.savefig("weights/" + str(cnt) + ".png")
+                plt.clf()
+
+        print(cnt)
+        return self.gm.predict(data), loss, self.hedge.probabilities
--- GMMs/train.py
+++ GMMs/train.py
@@ -2,20 +2,20 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-from sklearn import cluster, datasets
+from sklearn import datasets
 from sklearn.mixture import GaussianMixture
 from itertools import cycle, islice
 
-from cvar_em import CVaR_EM
+from cvar_em import CVarEM
 
 np.random.seed(31415)
 
 param = {
-    "alpha": 0.3,
-    "lr_hedge": 0.01,
+    "alpha": 0.6,
+    "lr_hedge": 0.1,
     "n_samples": 200,
     "n_initialization": 20,
-    "n": 100,
+    "n": 1,
     "dir": ["data", "output", "loss", "weights"],
     "path_X": "data/data_X.npy",
     "path_y": "data/data_y.npy",
@@ -25,7 +25,7 @@ param = {
 }
 
 
-def generate(n, n_samples, pathX, pathy):
+def generate(n, n_samples, path_x, path_y):
     """Generates the dataset and saves it as a numpy array.
 
     Parameters
@@ -33,37 +33,37 @@ def generate(n, n_samples, pathX, pathy):
     n: int
         Number of datasets to generate
     n_samples: int
-        Number of datapoints per dataset
-    pathX: string
+        Number of data points per dataset
+    path_x: string
         Path to save the dataset
-    pathy: string
-        Path to save the clusters of the correponding datapoints
+    path_y: string
+        Path to save the clusters of the corresponding data points
     """
 
     data = np.empty((n, n_samples + 1, 2))
-    Y = np.empty((n, n_samples))
-    for i in range(n):
+    data_y = np.empty((n, n_samples))
+    for j in range(n):
         k = np.random.randint(2, 5)
-        sample_distr = np.zeros(k)
-        while sum(sample_distr) != n_samples:
-            sample_distr = np.random.dirichlet(np.random.rand(k), 1)[0]
-            sample_distr = np.round(n_samples * sample_distr).astype(int)
+        sample_distribution = np.zeros(k)
+        while sum(sample_distribution) != n_samples:
+            sample_distribution = np.random.dirichlet(np.random.rand(k), 1)[0]
+            sample_distribution = np.round(n_samples * sample_distribution).astype(int)
         std = 4 * np.random.rand(k)
-        centers = 10 * np.random.rand(k, 2)
-        X, y = datasets.make_blobs(n_samples=n_samples, centers=k, cluster_std=std)
+        # centers = 10 * np.random.rand(k, 2)
+        x_cur, y_cur = datasets.make_blobs(n_samples=n_samples, centers=k, cluster_std=std)
         if np.random.rand() < 0.2:
             transformation = 2 * (np.random.rand(2, 2) - 0.5)
-            X = np.dot(X, transformation)
+            x_cur = np.dot(x_cur, transformation)
 
         # Normalize the data
-        X -= X.mean()
-        X /= X.std()
+        x_cur -= x_cur.mean()
+        x_cur /= x_cur.std()
 
-        data[i, :-1] = X
-        data[i, -1] = [k, 0]
-        Y[i] = y
-    np.save(pathX, data)
-    np.save(pathy, Y)
+        data[j, :-1] = x_cur
+        data[j, -1] = [k, 0]
+        data_y[i] = y_cur
+    np.save(path_x, data)
+    np.save(path_y, data_y)
 
 
 # Create directories for the output if they do not exist
@@ -81,15 +81,15 @@ fig = plt.figure(figsize=[10, 6])
 for i in range(param["n"]):
     print(i)
     curX = X[i, :-1]
-    cury = y[i]
+    cur_y = y[i]
     n_clusters = int(X[i, -1, 0])
 
-    cvar = CVaR_EM(
+    cvar = CVarEM(
         n_components=n_clusters,
         n_init=param["n_initialization"],
         num_actions=param["n_samples"],
         size=int(np.ceil(param["alpha"] * param["n_samples"])),
-        lr=param["lr_hedge"],
+        # lr=param["lr_hedge"],
     )
 
     gmm = GaussianMixture(
@@ -123,7 +123,7 @@ for i in range(param["n"]):
     # Plot the result
     ax = plt.subplot(1, 3, 1)
     ax.set_title("True distribution")
-    ax.scatter(curX[:, 0], curX[:, 1], s=10, color=colors[cury])
+    ax.scatter(curX[:, 0], curX[:, 1], s=10, color=colors[cur_y])
     ax1 = plt.subplot(1, 3, 2)
     ax1.set_title("GMM")
     ax1.scatter(curX[:, 0], curX[:, 1], s=10, color=colors[gmm_y])
