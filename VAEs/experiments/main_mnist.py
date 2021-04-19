2e58b9068241bb179ffb79a4d7e5b393baccf46c
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-04-19 17:30:42 +0200

    Optimize cvar loss for the mnist dataset

--- VAEs/experiments/main_mnist.py
+++ VAEs/experiments/main_mnist.py
@@ -3,40 +3,76 @@ import os
 import torch
 
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from torchvision.utils import save_image
 
-from cvarVAE.train import VAEalg, RockarfellarAlg, CVaRalg
+from cvarVAE.train import VaeAlg, Rockarfellar, AdaCVar
 
 seed = 31415
 np.random.seed(seed)
 torch.manual_seed(seed)
 
+
+class ImbalancedMNIST(Dataset):
+    def __init__(self, root, train, download, transform):
+        self.dataset = datasets.MNIST(root=root, download=download, transform=transform)
+        self.train = train
+        self.class_distr = np.array([0.8 ** i for i in range(10)])
+        self.class_distr /= np.sum(self.class_distr)
+        self.idx = self.resample()
+
+    def resample(self):
+        targets = self.dataset.train_labels if self.train else self.dataset.test_labels
+        _, class_counts = np.unique(targets, return_counts=True)
+        # Get class indices for resampling
+        class_indices = [np.where(targets == i)[0] for i in range(10)]
+        # Get class indices for reduced class count
+        idx = []
+        for i in range(10):
+            cur_count = int(class_counts[i] * self.class_distr[i])
+            idx.append(class_indices[i][:cur_count])
+        idx = np.hstack(idx)
+        np.random.shuffle(idx)
+        return idx
+
+    def __getitem__(self, index):
+        img, target = self.dataset[self.idx[index]]
+        return img, target
+
+    def __len__(self):
+        return len(self.idx)
+
+
 # learning params
 model_param = {
     "x_dim": 1,
-    "hidden_dims": [512],
+    "hidden_dims": [128, 128],
     "z_dim": 16,
-    "beta": 0.0,
     "constrained_output": True,
 }
 
+exp3_param = {"gamma": 0.9, "beta": 0.0, "eps": 0.0, "iid_batch": False}
+
 param = {
-    "epochs": 5,
-    "batch_size": 64,
-    "lr": 0.0001,
+    "epochs": 1000,
+    "batch_size": 256,
+    "lr": 1e-4,
     "alpha": 0.3,
     "beta": 1.0,
     "print": True,
-    "model_name": "VAEimg",
+    "model_name": "VaeImg",
+    "model_name_usual": "VAE usual",
+    "model_name_rockar": "Rockarfellar alg",
+    "model_name_ada": "AdaCVar alg",
     "save_model": True,
     "dir": ["../models/mnist/", "../output/out_mnist/", "../input/mnist/"],
     "path_data": "../input/mnist/",
     "path_vae": "../models/mnist/vae",
-    "path_cvar": "../models/mnist/cvar",
+    "path_rockar": "../models/mnist/rockar",
+    "path_ada": "../models/mnist/ada",
     "path_out": "../output/out_mnist/",
 }
-criterion = torch.nn.BCELoss(reduction="none")
+criterion = torch.nn.MSELoss(reduction="none")
 
 # Create directories for the output if they do not exist
 for cur_dir in param["dir"]:
@@ -44,17 +80,14 @@ for cur_dir in param["dir"]:
         os.makedirs(cur_dir)
 
 # Generate data
-train_set = datasets.MNIST(
+train_set = ImbalancedMNIST(
     root=param["path_data"], train=True, download=True, transform=transforms.ToTensor()
 )
-valid_set = datasets.MNIST(
+valid_set = ImbalancedMNIST(
     root=param["path_data"], train=False, download=True, transform=transforms.ToTensor()
 )
 
-train_loader_vae = DataLoader(train_set, batch_size=param["batch_size"], shuffle=True)
-val_loader = DataLoader(valid_set, batch_size=param["batch_size"], shuffle=True)
-
-vae = VAEalg(
+vae = VaeAlg(
     param["model_name"],
     param["path_vae"],
     model_param,
@@ -67,9 +100,9 @@ vae = VAEalg(
 )
 vae.train(param["epochs"], param["save_model"], param["print"])
 
-cvar = RockarfellarAlg(
+rockar = Rockarfellar(
     param["model_name"],
-    param["path_cvar"],
+    param["path_rockar"],
     model_param,
     train_set,
     valid_set,
@@ -79,20 +112,36 @@ cvar = RockarfellarAlg(
     param["alpha"],
     beta=param["beta"],
 )
-cvar.train(param["epochs"], param["save_model"], param["print"])
+rockar.train(param["epochs"], param["save_model"], param["print"])
+
+ada = AdaCVar(
+    param["model_name"],
+    param["path_ada"],
+    model_param,
+    exp3_param,
+    train_set,
+    valid_set,
+    param["batch_size"],
+    param["lr"],
+    criterion,
+    param["alpha"],
+    param["beta"],
+)
+ada.train(param["epochs"], param["save_model"], param["print"])
+
 
 vae.model.eval()
-cvar.model.eval()
+rockar.model.eval()
+ada.model.eval()
 with torch.no_grad():
-    for idx, (data, _) in enumerate(vae.val_loader):
-        data = data.to(vae.device)
-        data = data.view(data.size(0), -1)
-        recons, _, _ = vae.model(data)
-        recons_cvar, _, _ = cvar.model(data)
+    recons = vae.model.sample(64, vae.device)
+    recons_rockar = rockar.model.sample(64, rockar.device)
+    recons_ada = rockar.model.sample(64, rockar.device)
 
-        if idx % 5 == 0:
-            orig = data.view(data.shape[0], 1, 28, 28)
-            vae_img = recons.view(data.shape[0], 1, 28, 28)
-            cvar_img = recons_cvar.view(data.shape[0], 1, 28, 28)
-            all_img = torch.cat((orig[:8], vae_img[:8], cvar_img[:8]))
-            save_image(all_img.cpu(), f"{param['path_out']}output{idx}.png", nrow=8)
+    vae_img = recons.view(64, 1, 28, 28)
+    rockar_img = recons_rockar.view(64, 1, 28, 28)
+    ada_img = recons_ada.view(64, 1, 28, 28)
+
+    save_image(vae_img.cpu(), f"{param['path_out']}vae.png", nrow=8)
+    save_image(rockar_img.cpu(), f"{param['path_out']}rockarfellar.png", nrow=8)
+    save_image(ada_img.cpu(), f"{param['path_out']}adacvar.png", nrow=8)
