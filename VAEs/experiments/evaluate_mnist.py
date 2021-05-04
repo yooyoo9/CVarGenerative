f180a8f08c3e44ed3abbb2ca8eb26b20a00a7f08
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-05-04 08:27:25 +0200

    Removed dead code

--- VAEs/experiments/evaluate_mnist.py
+++ VAEs/experiments/evaluate_mnist.py
@@ -15,13 +15,12 @@ seed = 31415
 np.random.seed(seed)
 torch.manual_seed(seed)
 
-classifier = MnistClassifier()
-
 
 def generate_classes(model, device, path_out):
     model.eval()
     with torch.no_grad():
         data = model.sample(1000, device)
+    classifier = MnistClassifier()
     pred_labels = classifier.predict(data)
     data = data.cpu().numpy()
     recons = np.empty((100, 1, 28, 28))
@@ -32,7 +31,7 @@ def generate_classes(model, device, path_out):
         cur = data[idx[:10]]
         if len(cur) < 10:
             cur = np.concatenate((cur, np.zeros((10 - len(cur), 1, 28, 28))))
-        recons[10 * j : 10 * (j + 1)] = cur
+        recons[10 * j: 10 * (j + 1)] = cur
     img = torch.from_numpy(recons).view(100, 1, 28, 28)
     save_image(img, path_out + ".png", nrow=10)
     return classes
@@ -54,7 +53,7 @@ def plot_manifold(model, device, path_out):
             )
             x_decoded = model.decode(z)
             digit = torch.reshape(x_decoded[0], (28, 28))
-            image[28 * i : 28 * (i + 1), 28 * j : 28 * (j + 1)] = (
+            image[28 * i: 28 * (i + 1), 28 * j: 28 * (j + 1)] = (
                 digit.detach().cpu().numpy()
             )
     plt.figure(figsize=(10, 10))
@@ -64,60 +63,32 @@ def plot_manifold(model, device, path_out):
     plt.close()
 
 
-model_param = {
-    "x_dim": 1,
-    "hidden_dims": [512, 512],  # TODO
-    "z_dim": 2,
-    "constrained_output": True,
-}
+def evaluate(imbalanced):
+    name_out = "mod" if imbalanced else "usual"
+    dataset = ImbalancedMNIST if imbalanced else datasets.MNIST
 
-exp3_param = {"gamma": 0.9, "beta": 0.0, "eps": 0.0, "iid_batch": False}
+    param = {
+        "path_data": "../input/mnist/",
+        "path_model": "../models/mnist_" + name_out + "/",
+        "path_out": "../output/out_mnist_" + name_out + "/",
+        "path_true": "../output/out_mnist_" + name_out + "/true_data/",
+    }
 
-param = {
-    "imbalanced": True,
-    "epochs": 1000,
-    "batch_size": 256,
-    "lr": 1e-4,
-    "alpha": 0.3,
-    "beta": 1.0,
-    "print": True,
-    "model_name": "VaeImg",
-    "model_name_usual": "VAE usual",
-    "model_name_rockar": "Rockarfellar alg",
-    "model_name_ada": "AdaCVar alg",
-    "save_model": True,
-}
+    names = {
+        "vae": "Usual VAE",
+        "rockar": "Rockarfellar VAE",
+        "ada": "AdaCVaR VAE",
+    }
 
-name_out = "mod" if param["imbalanced"] else "usual"
-dataset = ImbalancedMNIST if param["imbalanced"] else datasets.MNIST
-
-out_param = {
-    "path_data": "../input/mnist/",
-    "path_model": "../models/mnist_" + name_out + "/",
-    "path_out": "../output/out_mnist_" + name_out + "/",
-    "path_true": "../output/out_mnist_" + name_out + "/true_data/",
-}
-
-names = {
-    "vae": "Usual VAE",
-    "rockar": "Rockarfellar VAE",
-    "ada": "AdaCVaR VAE",
-}
-
-
-criterion = torch.nn.MSELoss(reduction="none")
-
-
-def evaluate():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     n = 1000
-    output_file = open(out_param["path_out"] + "output.txt", "w")
+    output_file = open(param["path_out"] + "output.txt", "w")
 
-    if not os.path.exists(out_param["path_true"]):
+    if not os.path.exists(param["path_true"]):
         # Generate true images
-        os.makedirs(out_param["path_true"])
+        os.makedirs(param["path_true"])
         valid_set = dataset(
-            root=out_param["path_data"],
+            root=param["path_data"],
             train=False,
             download=True,
             transform=transforms.ToTensor(),
@@ -125,7 +96,7 @@ def evaluate():
         true_ratio = np.zeros(10)
         idxs = np.random.choice(len(valid_set), n, False)
         for i in idxs:
-            save_image(valid_set[i][0], out_param["path_true"] + str(i) + ".png")
+            save_image(valid_set[i][0], param["path_true"] + str(i) + ".png")
             true_ratio[valid_set[i][1]] += 1
         output_file.write("True class ratio: ")
         np.savetxt(output_file, true_ratio, newline=" ", fmt="%4d")
@@ -134,10 +105,10 @@ def evaluate():
     for cur in ["vae", "rockar", "ada"]:
         output_file.write(names[cur] + "\n")
         model = torch.load(
-            out_param["path_model"] + cur, map_location=torch.device("cpu")
+            param["path_model"] + cur, map_location=torch.device("cpu")
         ).to(device)
-        path_out_data = out_param["path_out"] + cur + "_data/"
-        path_out = out_param["path_out"] + cur
+        path_out_data = param["path_out"] + cur + "_data/"
+        path_out = param["path_out"] + cur
 
         model.eval()
         class_ratio = generate_classes(model, device, path_out)
@@ -169,4 +140,6 @@ def evaluate():
     output_file.close()
 
 
-evaluate()
+if __name__ == "__main__":
+    evaluate(imbalanced=False)
+    evaluate(imbalanced=True)
