ddd6c9c87ddabf55c5eff69e74e41ef9a2a98f32
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-05-21 18:23:59 +0200

    Changed fsize to fontsize

--- GMMs/evaluate.py
+++ GMMs/evaluate.py
@@ -16,7 +16,7 @@ colors = np.array(
     ]
 )
 
-fsize = 16
+fontsize = 16
 for i in range(5):
     cur = np.load("output/data" + str(i) + "_predictions.npy")
     true_y = cur[0]
@@ -26,19 +26,19 @@ for i in range(5):
 
     fig = plt.figure(figsize=[6, 3])
     ax = plt.subplot(1, 3, 1)
-    ax.set_title("True distribution", fontsize=fsize)
+    ax.set_title("True distribution", fontsize=fontsize)
     ax.axis("equal")
     ax.scatter(data[:, 0], data[:, 1], s=10, color=colors[true_y])
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
     ax1 = plt.subplot(1, 3, 2)
-    ax1.set_title("Usual EM", fontsize=fsize)
+    ax1.set_title("Usual EM", fontsize=fontsize)
     ax1.axis("equal")
     ax1.scatter(data[:, 0], data[:, 1], s=10, color=colors[gmm_y])
     ax1.xaxis.set_visible(False)
     ax1.yaxis.set_visible(False)
     ax2 = plt.subplot(1, 3, 3)
-    ax2.set_title("CVaR-EM", fontsize=fsize)
+    ax2.set_title("CVaR-EM", fontsize=fontsize)
     ax2.axis("equal")
     ax2.scatter(data[:, 0], data[:, 1], s=10, color=colors[cvar_y])
     ax2.xaxis.set_visible(False)
