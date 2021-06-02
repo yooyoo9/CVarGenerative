18ee3cd1bcbc130124b5ac1302c70e31c2ec0b7f
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-06-02 13:21:40 +0200

    Updated figures for GMMs for latex

--- GMMs/evaluate.py
+++ GMMs/evaluate.py
@@ -1,5 +1,7 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
+from figure import *
 
 X = np.load("data/data_X.npy")
 colors = np.array(
@@ -16,7 +18,6 @@ colors = np.array(
     ]
 )
 
-fontsize = 16
 for i in range(5):
     cur = np.load("output/data" + str(i) + "_predictions.npy")
     true_y = cur[0]
@@ -24,25 +25,22 @@ for i in range(5):
     cvar_y = cur[2]
     data = X[i, :-1]
 
-    fig = plt.figure(figsize=[6, 3])
-    ax = plt.subplot(1, 3, 1)
-    ax.set_title("True distribution", fontsize=fontsize)
+    set_figure_params(serif=False, fontsize=9)
+    fig, (ax, ax1, ax2) = plt.subplots(ncols=3, nrows=1)
+    fig.set_size_inches(6.75 / 2, 2.0)
+    ax.set_title("True distribution")
     ax.axis("equal")
     ax.scatter(data[:, 0], data[:, 1], s=10, color=colors[true_y])
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-    ax1 = plt.subplot(1, 3, 2)
-    ax1.set_title("Usual EM", fontsize=fontsize)
+    hide_all_ticks(ax)
+    ax1.set_title("Usual EM")
     ax1.axis("equal")
     ax1.scatter(data[:, 0], data[:, 1], s=10, color=colors[gmm_y])
-    ax1.xaxis.set_visible(False)
-    ax1.yaxis.set_visible(False)
-    ax2 = plt.subplot(1, 3, 3)
-    ax2.set_title("CVaR-EM", fontsize=fontsize)
+    hide_all_ticks(ax1)
+    ax2.set_title("CVaR-EM")
     ax2.axis("equal")
     ax2.scatter(data[:, 0], data[:, 1], s=10, color=colors[cvar_y])
-    ax2.xaxis.set_visible(False)
-    ax2.yaxis.set_visible(False)
+    hide_all_ticks(ax2)
+    adapt_figure_size_from_axes((ax, ax1, ax2))
     fig.tight_layout()
     plt.savefig("output/data" + str(i) + "_img.png")
     plt.clf()
