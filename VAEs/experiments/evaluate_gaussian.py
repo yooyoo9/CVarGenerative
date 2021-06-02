f3ee5dfbe9421106babc7dd24b1faa94043ebb55
Yunshu <yunshu.ouyang@inf.ethz.ch>
2021-06-02 13:24:10 +0200

    VAE gaussians - fix code and refractor

--- VAEs/experiments/evaluate_gaussian.py
+++ VAEs/experiments/evaluate_gaussian.py
@@ -1,53 +1,52 @@
 import numpy as np
 import os
 import torch
-import matplotlib
+import argparse
 import matplotlib.pyplot as plt
 
 from sklearn.mixture import GaussianMixture
+
+from data_gaussian import GaussianDataSet
+from util.figure import *
 
 seed = 31415
 np.random.seed(seed)
 torch.manual_seed(seed)
 
+models = ["true", "vae", "rocka", "ada"]
+name = ["True data", "VAE", "Rockafellar", "CVaR"]
 
-def evaluate_output(idx, vae, rocka, ada, device, alpha, dataset):
-    names = {
-        "usual": "VAE",
-        "rocka": "Rockafellar VAE",
-        "ada": "CVar-VAE",
-    }
-    path_out = "../output/out_gaussian/"
-
+def evaluate(dataset, idx, alpha, path_model, path_out):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    output_file = open(path_out + "output" + idx + ".txt", "w")
     true_data = dataset.data
     n_clusters = int(dataset.n_clusters)
     k_cvar = int(np.round(alpha * len(true_data)))
-    output_file = open(path_out + "output" + str(idx) + ".txt", "w")
-
-    vae.eval()
-    rocka.eval()
-    ada.eval()
-    with torch.no_grad():
-        recons = vae.sample(true_data.shape[0], device)
-        recons_rocka = rocka.sample(true_data.shape[0], device)
-        recons_ada = ada.sample(true_data.shape[0], device)
 
     gmm = GaussianMixture(
         n_components=n_clusters,
         n_init=20,
         random_state=seed,
     )
-    data = [true_data, recons, recons_rocka, recons_ada]
-    np.save(path_out + "data" + str(idx) + "_predictions.npy", np.array(data))
-    name = ["True data", "VAE", "Rockafellar VAE", "CVaR-VAE"]
+    recons = [true_data]
+    for i in range(1, 4):
+        cur = models[i]
+        model = torch.load(
+            path_model + cur, map_location=torch.device("cpu")
+        ).to(device)
+        model.eval()
+        with torch.no_grad():
+            data = model.sample(true_data.shape[0], device)
+        recons.append(data)
+    np.save(path_out + "recons" + idx + ".npy", np.array(recons))
+
     ll_true = np.empty(4)
     ll_cur = np.empty(4)
     for i in range(4):
-        gmm.fit(data[i])
-        cur = -gmm.score_samples(true_data)
-        ll_true[i] = np.mean(cur)
-        cur = -gmm.score_samples(data[i])
-        ll_cur[i] = np.mean(cur)
+        gmm.fit(recons[i])
+        ll_true[i] = np.mean(-gmm.score_samples(true_data))
+        ll_cur[i] = np.mean(-gmm.score_samples(recons[i]))
+
     output_file.write("Mean NLL of the true-data given reconstruction\n")
     for i in range(4):
         output_file.write(f"{name[i]}: {ll_true[i]:.2f}\n")
@@ -57,35 +56,38 @@ def evaluate_output(idx, vae, rocka, ada, device, alpha, dataset):
         output_file.write(f"{name[i]}: {ll_cur[i]:.2f}\n")
     output_file.close()
 
-    fig = plt.figure(figsize=[8, 3])
-    ax1 = plt.subplot(141)
-    ax2 = plt.subplot(142, sharex=ax1, sharey=ax1)
-    ax3 = plt.subplot(143, sharex=ax1, sharey=ax1)
-    ax4 = plt.subplot(144, sharex=ax1, sharey=ax1)
-    recons = recons.cpu()
-    recons_rocka = recons_rocka.cpu()
-    recons_ada = recons_ada.cpu()
-    fontsize = 18
-    ax1.scatter(true_data[:, 0], true_data[:, 1], s=10, color="black")
-    ax1.set_title("Validation data", {"fontsize": fontsize})
-    # ax1.axis("equal")
-    ax1.xaxis.set_visible(False)
-    ax1.yaxis.set_visible(False)
-    ax2.scatter(recons[:, 0], recons[:, 1], s=10, color="black")
-    ax2.set_title(names["usual"], {"fontsize": fontsize})
-    # ax2.axis("equal")
-    ax2.xaxis.set_visible(False)
-    ax2.yaxis.set_visible(False)
-    ax3.scatter(recons_rocka[:, 0], recons_rocka[:, 1], s=10, color="black")
-    ax3.set_title(names["rocka"], {"fontsize": fontsize})
-    # ax3.axis("equal")
-    ax3.xaxis.set_visible(False)
-    ax3.yaxis.set_visible(False)
-    ax4.scatter(recons_ada[:, 0], recons_ada[:, 1], s=10, color="black")
-    ax4.set_title(names["ada"], {"fontsize": fontsize})
-    # ax4.axis("equal")
-    ax4.xaxis.set_visible(False)
-    ax4.yaxis.set_visible(False)
+    set_figure_params(serif=False, fontsize=9)
+    fig, axes = plt.subplots(ncols=4, nrows=1, sharex=True, sharey=True)
+    fig.set_size_inches(6.75/2, 2.0)
+    for i in range(4):
+        cur_rec, ax, title = recons[i], axes[i], name[i]
+        if i != 0:
+            cur_rec = cur_rec.cpu()
+        ax.scatter(cur_rec[:, 0], cur_rec[:, 1], s=1, color="black")
+        ax.set_title(title)
+        hide_all_ticks(ax)
+    adapt_figure_size_from_axes(axes)
     fig.tight_layout()
-    plt.savefig(path_out + "output" + str(idx) + ".png")
-    plt.clf()
+    plt.savefig(path_out + "output" + idx + ".png")
+    plt.show()
+    plt.close()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=int, default=0, choices=set((0,1,2,3,4)))
+parser.add_argument("--alpha", type=float, default=0.3)
+parser.add_argument("--path_data", type=str, default="../input/gaussian/data.npy")
+parser.add_argument("--path_model", type=str, default="../models/gaussian/")
+parser.add_argument("--path_out", type=str, default="../output/gaussian/")
+args = parser.parse_args()
+
+args.path_model += str(args.dataset)
+dataset = GaussianDataSet(args.path_data, args.dataset, train=False)
+
+evaluate(
+    dataset,
+    str(args.dataset),
+    args.alpha,
+    args.path_model,
+    args.path_out,
+)
+    
--- VAEs/experiments/main_gaussian.py
+++ VAEs/experiments/main_gaussian.py
@@ -1,143 +1,92 @@
 import numpy as np
 import os
+import argparse
 
 import torch
-from torch.utils.data import Dataset
 
 from util.train import VaeAlg, Rockafellar, AdaCVar
-from data_gaussian import generate_data
-from evaluate_gaussian import evaluate_output
+from data_gaussian import GaussianDataSet
 
 seed = 764003779
 np.random.seed(seed)
 torch.manual_seed(seed)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs_vae", type=int, default=200)
+parser.add_argument("--epochs_rocka", type=int, default=200)
+parser.add_argument("--epochs_ada", type=int, default=200)
+parser.add_argument("--learning_rate", type=float, default=0.0001)
+parser.add_argument("--batch_size", type=int, default=256)
+parser.add_argument("--dataset", type=int, default=3, choices=set((0,1,2,3,4)))
+parser.add_argument("--alpha", type=float, default=0.3)
+parser.add_argument("--beta_vae", type=float, default=0.5)
+parser.add_argument("--beta_rocka", type=float, default=0.5)
+parser.add_argument("--beta_ada", type=float, default=0.5)
+#(0.6, 0.7, 0.6), (0.4, 0.5, 0.4), (0.5, 0.5, 0.4), 0.5, 0.5
+parser.add_argument("--print_loss", action='store_true', default=False)
+parser.add_argument("--save_model", action='store_true', default=False)
+parser.add_argument("--path_data", type=str, default="../input/gaussian/data.npy")
+parser.add_argument("--path_model", type=str, default="../models/gaussian/")
+args = parser.parse_args()
 
-class GaussianDataSet(Dataset):
-    def __init__(self, path, idx, train):
-        data = np.load(path)[idx]
-        input_data = data[:-1]
-        self.n_clusters = data[-1, 0]
-
-        # Normalize the data
-        input_data = input_data - input_data.mean(axis=0)
-        input_data = input_data / input_data.std(axis=0)
-
-        n_train = int(len(input_data) * 0.8)
-        if train:
-            # Training data
-            self.data = input_data[:n_train]
-        else:
-            # Validation data
-            self.data = input_data[n_train:]
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        cur = torch.tensor(self.data[idx]).type("torch.FloatTensor")
-        return cur, idx
-
+exp3_param = {"gamma": 0.8, "beta": 0.0, "eps": 0.0, "iid_batch": False}
+criterion = torch.nn.MSELoss(reduction="none")
 
 # learning params
 model_param = {
     "x_dim": 2,
     "hidden_dims": [128, 128],
     "z_dim": 2,
-    "constrained_output": False,
 }
-
-exp3_param = {"gamma": 0.8, "beta": 0.0, "eps": 0.0, "iid_batch": False}
-
-# 0.5, (0.4, 0.5, 0.4), 0.5, 0.5, 0.5, 0.5
-param = {
-    "epochs": 100,
-    "batch_size": 256,
-    "lr": 1e-4,
-    "alpha": 0.3,
-    "beta_usual": 0.5,
-    "beta_rocka": 0.5,
-    "beta_ada": 0.5,
-    "early_stop": 1000,
-    "print": True,
-    "model_name": "VAE",  # or VaeImg
-    "save_model": True,
-    "nb": 5,  # number of datasets
-    "data_size": 1000,
-    "dir": [
-        "../models/gaussian/",
-        "../output/out_gaussian/",
-        "../input/gaussian/",
-    ],
-    "path_data": "../input/gaussian/gaussians.npy",
-    "path_vae": "../models/gaussian/vae",
-    "path_rocka": "../models/gaussian/rocka",
-    "path_ada": "../models/gaussian/ada",
-    "path_out": "../output/out_gaussian/",
-}
-
-criterion = torch.nn.MSELoss(reduction="none")
 
 # Create directories for the output if they do not exist
-for cur_dir in param["dir"]:
-    if not os.path.exists(cur_dir):
-        os.makedirs(cur_dir)
+if not os.path.exists(args.path_model):
+    os.makedirs(args.path_model)
+args.path_model += str(args.dataset)
 
 # Generate data
-if not os.path.isfile(param["path_data"]):
-    generate_data(param["data_size"], param["path_data"])
+train_set = GaussianDataSet(args.path_data, args.dataset, train=True)
+valid_set = GaussianDataSet(args.path_data, args.dataset, train=False)
 
-for i in range(param["nb"]):
-    print(f"Dataset {i} of {param['nb']}")
-    train_set = GaussianDataSet(param["path_data"], i, train=True)
-    valid_set = GaussianDataSet(param["path_data"], i, train=False)
+vae = VaeAlg(
+    "VAE",
+    args.path_model,
+    model_param,
+    train_set,
+    valid_set,
+    args.batch_size,
+    args.learning_rate,
+    criterion,
+    args.beta_vae,
+)
 
-    vae = VaeAlg(
-        param["model_name"],
-        param["path_vae"] + str(i),
-        model_param,
-        train_set,
-        valid_set,
-        param["batch_size"],
-        param["lr"],
-        criterion,
-        param["beta_usual"],
-        param["early_stop"],
-    )
+rocka = Rockafellar(
+    "VAE",
+    args.path_model,
+    model_param,
+    train_set,
+    valid_set,
+    args.batch_size,
+    args.learning_rate,
+    criterion,
+    args.alpha,
+    args.beta_rocka,
+)
 
-    rocka = Rockafellar(
-        param["model_name"],
-        param["path_rocka"] + str(i),
-        model_param,
-        train_set,
-        valid_set,
-        param["batch_size"],
-        param["lr"],
-        criterion,
-        param["alpha"],
-        param["beta_rocka"],
-        param["early_stop"],
-    )
+ada = AdaCVar(
+    "VAE",
+    args.path_model,
+    model_param,
+    exp3_param,
+    train_set,
+    valid_set,
+    args.batch_size,
+    args.learning_rate,
+    criterion,
+    args.alpha,
+    args.beta_ada,
+)
 
-    ada = AdaCVar(
-        param["model_name"],
-        param["path_ada"] + str(i),
-        model_param,
-        exp3_param,
-        train_set,
-        valid_set,
-        param["batch_size"],
-        param["lr"],
-        criterion,
-        param["alpha"],
-        param["beta_ada"],
-        param["early_stop"],
-    )
-
-    vae.train(param["epochs"], param["save_model"], param["print"])
-    rocka.train(param["epochs"], param["save_model"], param["print"])
-    ada.train(param["epochs"], param["save_model"], param["print"])
-
-    evaluate_output(
-        i, vae.model, rocka.model, ada.model, vae.device, param["alpha"], valid_set
-    )
+vae.train(args.epochs_vae, args.save_model, args.print_loss)
+rocka.train(args.epochs_rocka, args.save_model, args.print_loss)
+ada.train(args.epochs_ada, args.save_model, args.print_loss)
